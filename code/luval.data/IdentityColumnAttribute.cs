e065d7d9bd6b9f522261bef39dbf03410e49c482
marinoscar <oscar@marin.cr>
2019-08-30 21:39:58 -0500

    added new attributes

---
+++ code/luval.data/IdentityColumnAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace luval.data
+{
+    /// <summary>
+    /// Specifies that a column is an identity auto increment
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class IdentityColumnAttribute : Attribute
+    {
+    }
+}
---
+++ code/luval.data/PrimaryKeyAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace luval.data
+{
+    /// <summary>
+    /// Specifies that a column is a primary key
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class PrimaryKeyAttribute : Attribute
+    {
+    }
+}
