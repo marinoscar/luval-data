e6d96650c31af578bef2b6d675c19a4e515f39b5
marinoscar <oscar@marin.cr>
2019-08-30 20:53:41 -0500

    added a new test case

--- code/luval.data.tests/When_Entities_Are_Being_Map.cs
+++ code/luval.data.tests/When_Entities_Are_Being_Map.cs
@@ -54,5 +54,54 @@ namespace luval.data.tests
             Assert.AreEqual(entity.Value, res.Single().Value);
             Assert.AreEqual(entity.UpdatedOn, res.Single().UpdatedOn);
         }
+
+        [Test]
+        public void It_Should_Ignore_Fields_Not_Present_In_The_Entity()
+        {
+            var entity = new Simple()
+            {
+                Id = new Random().Next(),
+                Name = "Sample Text",
+                Value = new Random().NextDouble(),
+                UpdatedOn = DateTime.Today
+            };
+            var record = new DictionaryDataRecord(new Dictionary<string, object>() {
+                { "Id", entity.Id },
+                { "Name", entity.Name },
+                { "Value", entity.Value },
+                { "UpdatedOn", entity.UpdatedOn },
+                { "ExtraField", Guid.NewGuid() },
+            });
+            var db = new Database(() => new MConnection() { Records = new List<IDataRecord>(new[] { record }) });
+            var res = db.ExecuteToEntityList<Simple>("");
+            Assert.AreEqual(entity.Id, res.Single().Id);
+            Assert.AreEqual(entity.Name, res.Single().Name);
+            Assert.AreEqual(entity.Value, res.Single().Value);
+            Assert.AreEqual(entity.UpdatedOn, res.Single().UpdatedOn);
+        }
+
+        [Test]
+        public void It_Should_Not_Fields_That_Are_Not_Named_Correctly()
+        {
+            var entity = new Simple()
+            {
+                Id = new Random().Next(),
+                Name = "Sample Text",
+                Value = new Random().NextDouble(),
+                UpdatedOn = DateTime.Today
+            };
+            var record = new DictionaryDataRecord(new Dictionary<string, object>() {
+                { "WrongId", entity.Id },
+                { "Name", entity.Name },
+                { "Value", entity.Value },
+                { "UpdatedOn", entity.UpdatedOn },
+            });
+            var db = new Database(() => new MConnection() { Records = new List<IDataRecord>(new[] { record }) });
+            var res = db.ExecuteToEntityList<Simple>("");
+            Assert.AreEqual(0, res.Single().Id, "Need to match the default value of the Id field");
+            Assert.AreEqual(entity.Name, res.Single().Name);
+            Assert.AreEqual(entity.Value, res.Single().Value);
+            Assert.AreEqual(entity.UpdatedOn, res.Single().UpdatedOn);
+        }
     }
 }
