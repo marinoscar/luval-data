107b475d68c6918ab06e7814c404430658418da3
marinoscar <oscar@marin.cr>
2019-08-30 21:40:36 -0500

    added a class to persist an entity in a database

---
+++ code/luval.data/EntityAdapter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace luval.data
+{
+    public class EntityAdapter<T>
+    {
+        public Database Database { get; private set; }
+        public T Entity { get; private set; }
+
+        public int Insert()
+        {
+            return 0;
+        }
+
+        public int Update()
+        {
+            return 0;
+        }
+
+        public int Delete()
+        {
+            return 0;
+        }
+
+
+    }
+}
