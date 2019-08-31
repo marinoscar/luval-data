a1fceb670be98908dae065b91e3185856860b19e
marinoscar <oscar@marin.cr>
2019-08-30 21:55:57 -0500

    added support to load the schema of a table using an entity class and custom attributes, also created test cases to validate it works properly

--- code/luval.data/SqlTableSchema.cs
+++ code/luval.data/SqlTableSchema.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace luval.data
@@ -7,7 +8,24 @@ namespace luval.data
     public class SqlTableSchema
     {
         public string TableName { get; set; }
-        public string DataSchema { get; set; }
         public List<SqlColumnSchema> Columns { get; set; }
+
+        public static SqlTableSchema Load(Type type)
+        {
+            var columns = new List<SqlColumnSchema>();
+            foreach (var prop in type.GetProperties())
+            {
+                if (prop.GetCustomAttribute<NotMappedAttribute>() != null) continue;
+                columns.Add(SqlColumnSchema.Load(prop));
+            }
+            return new SqlTableSchema() { TableName = GetTableName(type), Columns = columns };
+        }
+
+        private static string GetTableName(Type type)
+        {
+            var att = type.GetCustomAttribute<TableNameAttribute>();
+            if (att == null) return type.Name;
+            return att.Name;
+        }
     }
 }
