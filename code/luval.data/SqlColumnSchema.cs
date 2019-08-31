9e2467ea9ce7bf8f53ee866387eed63f8dccacfa
marinoscar <oscar@marin.cr>
2019-08-30 21:40:16 -0500

    added a way to collect the sql schema from an entity

---
+++ code/luval.data/SqlColumnSchema.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace luval.data
+{
+    /// <summary>
+    /// Provides an abstraction for the schema of a sql column
+    /// </summary>
+    public class SqlColumnSchema
+    {
+        public string Name { get; set; }
+        public bool IsPrimaryKey { get; set; }
+        public bool IsIdentity { get; set; }
+
+        public static SqlColumnSchema Load(PropertyInfo property)
+        {
+            return new SqlColumnSchema()
+            {
+                Name = GetColumnName(property),
+                IsPrimaryKey = property.GetCustomAttribute<PrimaryKeyAttribute>() != null,
+                IsIdentity = property.GetCustomAttribute<PrimaryKeyAttribute>() != null
+            };
+        }
+
+        internal static string GetColumnName(PropertyInfo property)
+        {
+            var att = property.GetCustomAttribute<ColumnNameAttribute>();
+            if (att == null) return property.Name;
+            return ((ColumnNameAttribute)att).Name;
+        }
+    }
+}
---
+++ code/luval.data/SqlTableSchema.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace luval.data
+{
+    public class SqlTableSchema
+    {
+        public string TableName { get; set; }
+        public string DataSchema { get; set; }
+        public List<SqlColumnSchema> Columns { get; set; }
+    }
+}
