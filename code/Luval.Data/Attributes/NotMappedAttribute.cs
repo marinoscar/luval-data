e512633f3bce46d85650fcd4e5b93b6cbeabd09b
marinoscar <oscar@marin.cr>
2021-01-27 14:11:38 -0600

    improved code added

---
+++ code/Luval.Data/Attributes/NotMappedAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Luval.Data.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class NotMappedAttribute : Attribute
+    {
+    }
+}
---
+++ code/Luval.Data/Entities/StringKeyAuditEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Luval.Data.Entities
+{
+    public abstract class StringKeyAuditEntity : AuditEntity<string>
+    {
+        public StringKeyAuditEntity() : base()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
