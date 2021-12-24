58ab53415166e360eec4e20a503b776c3dc00b33
Oscar Marin <oscar@marin.cr>
2021-12-24 01:30:45 -0600

    added more extension methods

--- code/Luval.Data/Extensions/IUnitOfWorkExtensions.cs
+++ code/Luval.Data/Extensions/IUnitOfWorkExtensions.cs
@@ -34,9 +34,62 @@ namespace Luval.Data.Extensions
             return await uow.SaveChangesAsync(cancellationToken);
         }
 
+        public static async Task<int> AddOrUpdateAndSaveIdEntityAsync<TEntity, TKey>(this IUnitOfWork<TEntity, TKey> uow, TEntity entity, CancellationToken cancellationToken) where TEntity : IIdBasedEntity<TKey>
+        {
+            var item = await uow.Entities.Query.GetAsync(i => i.Id != null && i.Id.Equals(entity.Id), cancellationToken);
+            if (item != null && !item.Any())
+                uow.Entities.Update(entity);
+            else
+                uow.Entities.Add(entity);
+
+            return await uow.SaveChangesAsync(cancellationToken);
+        }
+
+        public static Task<int> AddOrUpdateAndSaveAuditEntityAsync<TEntity, TKey>(this IUnitOfWork<TEntity, TKey> uow, TEntity entity, Expression<Func<TEntity, bool>> recordCondition, CancellationToken cancellationToken) where TEntity : IAuditableEntity<TKey>
+        {
+            return AddOrUpdateAndSaveAuditEntityAsync(uow, entity, recordCondition, null, cancellationToken);
+        }
+
+        public static async Task<int> AddOrUpdateAndSaveAuditEntityAsync<TEntity, TKey>(this IUnitOfWork<TEntity, TKey> uow, TEntity entity, Expression<Func<TEntity, bool>> recordCondition, string userId, CancellationToken cancellationToken) where TEntity : IAuditableEntity<TKey>
+        {
+            var item = await uow.Entities.Query.GetAsync(recordCondition, cancellationToken);
+            if (item != null && !item.Any())
+            {
+                entity.Id = item.First().Id;
+                entity.UtcUpdatedOn = DateTime.UtcNow;
+                entity.UpdatedByUserId = userId;
+                uow.Entities.Update(entity);
+            }
+            else
+            {
+                entity.UtcCreatedOn = DateTime.UtcNow;
+                entity.UtcUpdatedOn = entity.UtcCreatedOn;
+                entity.CreatedByUserId = userId;
+                entity.UpdatedByUserId = userId;
+                uow.Entities.Add(entity);
+            }
+            return await uow.SaveChangesAsync(cancellationToken);
+        }
+
+
+        public static int AddOrUpdateAndSaveAuditEntity<TEntity, TKey>(this IUnitOfWork<TEntity, TKey> uow, TEntity entity, Expression<Func<TEntity, bool>> recordCondition, string userId, CancellationToken cancellationToken) where TEntity : IAuditableEntity<TKey>
+        {
+            return AddOrUpdateAndSaveAuditEntityAsync(uow, entity, recordCondition, userId, CancellationToken.None).Result;
+        }
+
+        public static int AddOrUpdateAndSaveAuditEntity<TEntity, TKey>(this IUnitOfWork<TEntity, TKey> uow, TEntity entity, Expression<Func<TEntity, bool>> recordCondition, CancellationToken cancellationToken) where TEntity : IAuditableEntity<TKey>
+        {
+            return AddOrUpdateAndSaveAuditEntityAsync(uow, entity, recordCondition, null, CancellationToken.None).Result;
+        }
+
         public static int AddOrUpdateAndSave<TEntity, TKey>(this IUnitOfWork<TEntity, TKey> uow, TEntity entity, Expression<Func<TEntity, bool>> recordCondition)
         {
             return AddOrUpdateAndSaveAsync(uow, entity, recordCondition, CancellationToken.None).Result;
+        }
+
+        public static int AddOrUpdateAndSaveIdEntity<TEntity, TKey>(this IUnitOfWork<TEntity, TKey> uow, TEntity entity) where TEntity : IIdBasedEntity<TKey>
+        {
+            return AddOrUpdateAndSaveIdEntityAsync(uow, entity, CancellationToken.None).Result;
         }
 
         public static Task<int> RemoveAndSaveAsync<TEntity, TKey>(this IUnitOfWork<TEntity, TKey> uow, TEntity entity, CancellationToken cancellationToken)
